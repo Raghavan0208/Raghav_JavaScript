846f8f069b6af9640b0e1ad51180779dbefbf9b5
Raghavan <lookupraghav8111@gmail.com>
2026-01-07 02:46:19 -0500

    controlflow and conditional statement

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,45 @@
+let userRole = "admin";
+let accesslevel;
+
+if(userRole === "admin"){
+    accesslevel = "Full access granted";
+
+}else if (userRole === "manager"){
+    accesslevel = "Limited access";
+
+}else{
+    accesslevel = "no access";
+}
+console.log("AccessLevel : ",accesslevel);
+
+let isLoggedIn = true;
+let userMessage;
+
+if (isLoggedIn) {
+    if (userRole === "admin") {
+        userMessage = "Welcome, Admin!";
+    } else {
+        userMessage = "Welcome, User!";
+    }
+} else {
+    userMessage = "Please log in to access the system.";
+}
+console.log("User Message:", userMessage);
+
+let userType = "subscriber";
+let userCategory;
+
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+}
+console.log("User Category:", userCategory);
